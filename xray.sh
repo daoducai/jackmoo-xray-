9539f4919ee3526a5da5e4337814d9f310d42a20
jackmoo <51739356+a31663835@users.noreply.github.com>
2021-09-02 22:17:22 +0800

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -288,7 +288,7 @@ getData() {
                 colorEcho ${BLUE}  "${DOMAIN} 解析结果：${real_addr}"
 	    else
                 colorEcho ${RED}  " 域名未解析到当前服务器IP!"
-	        exit 1
+	        exit 
             fi
         fi
     fi

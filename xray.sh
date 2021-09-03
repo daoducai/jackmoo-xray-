e700d26f59c0633fbe2e98ca9e3ed3d3d45aff46
jackmoo <51739356+a31663835@users.noreply.github.com>
2021-09-03 09:07:01 +0800

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -282,10 +282,11 @@ getData() {
             KEY_FILE="/usr/local/etc/xray/${DOMAIN}.key"
         else
 	    read DOMAIN
-	    real_addr=`ping ${DOMAIN} -c 1 | sed '1{s/[^(]*(//;s/).*//;q}'`
-	    #local_addr=`curl v4.ident.me`
-	    local_addr=`curl ipv4.icanhazip.com`
-            if [ $real_addr == $local_addr ]; then
+	    real_addr=$(ping ${DOMAIN} -c 1 | sed '1{s/[^(]*(//;s/).*//;q}')
+	    #local_addr=$(curl -s v4.ident.me)
+	    #local_addr=$(curl -s https://ipinfo.io/ip)
+	    local_addr=$(curl -s ipv4.icanhazip.com)
+            if [ ${real_addr} == ${local_addr} ]; then
                 colorEcho ${BLUE}  "${DOMAIN} 解析结果：${real_addr}"
 	    else
                 colorEcho ${RED}  " 域名未解析到当前服务器IP!"

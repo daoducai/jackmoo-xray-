4957ceb5e32584ded6893e9d364312af953fad5f
jackmoo <51739356+a31663835@users.noreply.github.com>
2021-09-03 08:00:58 +0800

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -284,7 +284,7 @@ getData() {
 	    real_addr=`ping ${DOMAIN} -c 1 | sed '1{s/[^(]*(//;s/).*//;q}'`
 	    #local_addr=`curl v4.ident.me`
 	    local_addr=`curl ipv4.icanhazip.com`
-            if [ $real_addr == $local_addr ]; then
+            if [ [$real_addr == $local_addr ]]; then
                 colorEcho ${BLUE}  "${DOMAIN} 解析结果：${real_addr}"
 	    else
                 colorEcho ${RED}  " 域名未解析到当前服务器IP!"

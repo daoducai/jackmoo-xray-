719f45ac53881bc988970e263a95a0a337aafd84
jackmoo <51739356+a31663835@users.noreply.github.com>
2021-09-03 09:37:23 +0800

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -285,7 +285,7 @@ getData() {
             #res=`echo -n ${resolve} | grep ${IP}`
 	    real_ip=$(ping "${DOMAIN}" -c 1 | sed '1{s/[^(]*(//;s/).*//;q}')
 	    local_ip=$(curl -s https://ipinfo.io/ip)
-            if ["${real_ip}" = "${local_ip}" ]; then
+            if ["${real_ip}" == "${local_ip}" ]; then
                 colorEcho ${BLUE}  "${DOMAIN} 解析结果：${local_ip}"
 	    else
                 colorEcho ${RED}  " 域名未解析到当前服务器IP(${IP})!"
